df9a28059700ad5f8f770b6974cbdcbb3ce621da
Ashley Cho <ashleycho.slp@gmail.com>
2021-12-20 14:09:40 -0800

    integrate front end

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 //https://infinite-garden-80103.herokuapp.com/api/animals
+//http://localhost:3001
+
 const fs = require('fs');
 const path = require('path');
 const express = require('express');
@@ -11,6 +13,7 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
 app.use(express.json());
+app.use(express.static('public'));
 
 function filterByQuery(query, animalsArray) {
     let personalityTraitsArray = [];
@@ -112,8 +115,22 @@ app.post('/api/animals', (req, res) => {
     }
   });
 
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+  });
+
+app.get('/animals', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/animals.html'));
+  });
+
+app.get('/zookeepers', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+  });
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+  });
+
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
   });
-
-  
